57cc5775638d329a88ea2a373897960c035e95be
BrotherZemurro3 <konevichenko@icloud.com>
2025-03-24 16:41:36 +0300

    refact: Удален main.storyboard и начата его верстка кодом add: Создан Launchscreen по макету(логотип,цвет, констрейнты)

--- Tracker/AppDelegate.swift
+++ Tracker/AppDelegate.swift
@@ -11,10 +11,21 @@ import CoreData
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
+    var window: UIWindow?
+    
+    
+    
+    
+    init(window: UIWindow? = nil) {
+        self.window = window
+    }
+ 
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        window = UIWindow()
+        window?.rootViewController = ViewController()
+        window?.makeKeyAndVisible()
         return true
     }
 
