81131cbb542b2cb586bc3495952e0de7a1477666
BrotherZemurro3 <konevichenko@icloud.com>
2025-04-21 20:53:57 +0300

    upd: CoreData

--- Tracker/Services/СoreDataManager.swift
+++ Tracker/Services/СoreDataManager.swift
@@ -1,1 +1,39 @@
 
+import CoreData
+
+final class CoreDataManager {
+    static let shared = CoreDataManager
+    
+    
+    lazy var persistentContainer: NSPersistentCloudKitContainer = {
+
+        let container = NSPersistentCloudKitContainer(name: "Tracker")
+        container.loadPersistentStores(completionHandler: { (storeDescription, error) in
+            if let error = error as NSError? {
+               
+                fatalError("Unresolved error \(error), \(error.userInfo)")
+            }
+        })
+        return container
+    }()
+
+    var context: NSManagedObjectContext {
+        persistentContainer.viewContext
+    }
+    
+    // MARK: - Core Data Saving support
+
+    func saveContext () {
+        let context = persistentContainer.viewContext
+        if context.hasChanges {
+            do {
+                try context.save()
+            } catch {
+             
+                let nserror = error as NSError
+                fatalError("Unresolved error \(nserror), \(nserror.userInfo)")
+            }
+        }
+    }
+    
+}
