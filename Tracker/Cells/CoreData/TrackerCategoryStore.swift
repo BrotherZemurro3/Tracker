70b71cf1e4e1a3c452a2075972f963678d373f51
BrotherZemurro3 <konevichenko@icloud.com>
2025-04-29 17:28:25 +0300

    add: TrackerCategoryStore

--- Tracker/Cells/CoreData/TrackerCategoryStore.swift
+++ Tracker/Cells/CoreData/TrackerCategoryStore.swift
@@ -5,3 +5,30 @@
 //  Created by Дионисий Коневиченко on 29.04.2025.
 //
 import CoreData
+import Foundation
+
+
+final class TrackerCategoryStore {
+    private let context: NSManagedObjectContext
+    
+    init(context: NSManagedObjectContext = CoreDataManager.shared.context) {
+        self.context = context
+    }
+    
+    func createCategory(title: String) throws -> TrackerCategoryCoreData {
+        let entity = TrackerCategoryCoreData(context: context)
+        entity.title = title
+        try context.save()
+        return entity
+    }
+    
+    func fetchAllCategories() throws -> [TrackerCategoryCoreData] {
+        let request: NSFetchRequest<TrackerCategoryCoreData> = TrackerCategoryCoreData.fetchRequest()
+        return try context.fetch(request)
+    }
+    func category(withTitle title: String) throws -> TrackerCategoryCoreData? {
+        let request: NSFetchRequest<TrackerCategoryCoreData> = TrackerCategoryCoreData.fetchRequest()
+        request.predicate = NSPredicate(format: "title == %@", title)
+        return try context.fetch(request).first
+    }
+}
