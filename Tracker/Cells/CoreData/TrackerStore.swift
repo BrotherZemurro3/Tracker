7a41b68103e7d6062d0987a543bc025ea068378d
BrotherZemurro3 <konevichenko@icloud.com>
2025-04-29 17:18:12 +0300

    add: TrackerStore

--- Tracker/Cells/CoreData/TrackerStore.swift
+++ Tracker/Cells/CoreData/TrackerStore.swift
@@ -6,3 +6,33 @@
 //
 
 import CoreData
+import Foundation
+import CoreData
+import UIKit
+
+final class TrackerStore {
+    private let context: NSManagedObjectContext
+
+    init(context: NSManagedObjectContext = CoreDataManager.shared.context) {
+        self.context = context
+    }
+
+    func createTracker(from model: Tracker, category: TrackerCategoryCoreData) throws {
+        let entity = TrackerCoreData(context: context)
+        entity.id = model.id
+        entity.title = model.title
+        entity.colorHex = model.color.hexString
+        entity.emoji = model.emoji
+        entity.schedule = model.schedule?.map { String($0.rawValue) }.joined(separator: ",")
+        entity.isRegular = model.isRegular
+        entity.category = category
+        try context.save()
+    }
+
+    func fetchAllTrackers() throws -> [TrackerCoreData] {
+        let request: NSFetchRequest<TrackerCoreData> = TrackerCoreData.fetchRequest()
+        return try context.fetch(request)
+    }
+
+}
+
